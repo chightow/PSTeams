cdd832f62e7c85ec0f7e29c1b2b12b8d851d54a9
chightow <charleshightower@gmail.com>
2022-01-10 14:44:29 -0600

    Create TeamsFunctions.ps1

---
+++ TeamsFunctions.ps1
@@ -0,0 +1,20 @@
+Function Invoke-TeamsMSG {
+	param( [string[]]$To, [string]$Message ) 
+	$To = [system.web.httputility]::URLENCODE($To -join ',')
+	$Message = [system.web.httputility]::URLENCODE($Message)
+	$Command = "/l/chat/0/0?users=$To&TopicName=Blah&message=$Message"
+	$Null = start-process msteams:$Command 
+}
+Function Invoke-TeamsCall {
+	param ( [string[]]$To )
+	$To = [system.web.httputility]::URLENCODE($To -join ',')
+	$Command = "/l/call/0/0?users=$To"
+	$Null = start-process msteams:$Command
+}
+Function Invoke-TeamsScheduleMeeting {
+	param([string[]]$To,[string]$subject)
+	$Subject = [system.web.httputility]::URLENCODE($Subject)
+	$Attendees = [system.web.httputility]::URLENCODE($To -join ',')
+	$Command = "/l/meeting/new?subject=$Subject&attendees=$Attendees"
+	$Null = start-process msteams:$Command
+}
